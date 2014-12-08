7f7b447344bc88bb6aaa9f25fe5d7fc728940915
Zoher Ghadyali <zoher.ghadyali@gmail.com>
2014-12-07 19:28:37 -0500

    Added carrying capacity. Need to fix excess equations

--- honey_bee_iter1.m
+++ honey_bee_iter1.m
@@ -3,26 +3,33 @@ function res = honey_bee_iter1()
     d = 0.14;    %natural death rate
     L = 2000;
     w = 27000;
+    k = 0.25;    %rate at which worker bees respond to difference in H and F
+    B_c = 12500;      %baby bee carrying capacity
+    H_c = 35000;      %hive bee carrying capacity
+    F_c = 12500;      %forager bee carrying capacity
 
     function res = derivs(t, W)
         B = W(1);
         H = W(2);
-        F = W(3);       
-        dBdt = (L *((H+F)/(H+F+w)))*(1-B/20000);
-        %dHdt = (e*B + (F-H)*H)*(1-H/20000);
-        dHdt = (e*B)*(1-H/20000);
-        dFdt = ((H-F)*F - d*F)*(1-F/20000);
-        
+        F = W(3);
+        dBdt = (L *((H+F)/(H+F+w)))*(1-B/B_c);
+        dHdt = (e*B + k*(F-H))*(1-H/H_c);
+%         if F > H
+%             dHdt = (e*B + 0.75*(F-H) - 0.35*H) * (1-H/H_c);
+%         else
+%             dHdt = (e*B -0.35*H)*(1-H/H_c);
+%         end
+        dFdt = (-d*F + k*(H-F))*(1-F/F_c);
+
         res = [dBdt; dHdt; dFdt];
     end
         
-    [T,Y] = ode45(@derivs,[0,100],[0, 10, 500]);
+    [T,Y] = ode45(@derivs,[0,100],[0, 0, 1000]);
     figure;
     hold on
     plot(T,Y(:, 1), 'b')    %baby bee population
     plot(T, Y(:,2) + Y(:,3), 'y');  %worker bee population
     plot(T, Y(:, 2), 'r')
     plot(T, Y(:, 3), 'g')
-%    res = Y;
 
 end
