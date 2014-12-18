853644302fa93c9f45aa1ad2afc71e7c85561a1c
Zoher Ghadyali <zoher.ghadyali@gmail.com>
2014-12-17 23:33:25 -0500

    Cynthia is working her Linearity magic rn

--- honey_bee_iter3.m
+++ honey_bee_iter3.m
@@ -1,35 +1,43 @@
 function res = pde1()
     inf = 0;
-    ms = 0;
-    m = 0;
-    a_range = linspace(0, 1000000, 100);
+    ms = 0.5;
+    m = 1;
+    a_range = linspace(0, 21);
+    %a_range = linspace(0, 3);
     t_range = linspace(0, 100);
     
     sol = pdepe(m, @pdefun, @icfun, @bcfun, a_range, t_range);
     S = sol(:,:,1);
     
     figure;
-    surf(a_range, t_range, S);
-    xlabel('Age');
-    ylabel('Time');
+    surf(t_range, a_range, S);
+    ylabel('Age');
+    xlabel('Time');
     zlabel('Population');
-
+    %figure;
+    %plot(a_range, S(3, :));
     function[c, f, s] = pdefun(a, t, S, dSda)
-        c = 1/(-1/7);
+        %c = -7;
+        c = -1;
         f = S;
-        s = -inf*S/(-7) - m*S/(-7);
+        %s = -inf*S/(-7) - ms*S/(-7);
+        %s = -inf*S - ms*S;
+        s = -dSda;
     end
 
-    function S0 = icfun(a)
-        S0 = 60000;
+    function u0 = icfun(a) 
+        u0 = 60000*t;
     end
 
     function[p1, q1, p2, q2] = bcfun(xl, ul, xr, ur, t)
-        q1 = 1;
         p1 = -2000;
+        %p1 = 0;
+        q1 = 1;
 
-        p2 = 0;
-        q2 = 1;
+        p2 = ur;
+        q2 = 0;
+        %p2 = 0;
+        %q2 = 1;
     end
 
 end
